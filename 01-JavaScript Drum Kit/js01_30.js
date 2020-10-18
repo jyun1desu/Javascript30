de01211699068bf694f289e661cfeb60e3a236b2
jyun1 <jyuny12345@gmail.com>
2020-10-18 16:10:54 +0800

    Remove trasition and done

--- 01-JavaScript Drum Kit/js01_30.js
+++ 01-JavaScript Drum Kit/js01_30.js
@@ -1,9 +1,17 @@
-
-window.addEventListener("keydown",function(e){
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    if(!audio) return
+    if (!audio) return
     audio.currentTime = 0;
     audio.play();
     key.classList.add("playing")
-});
+};
+
+function removeTrainsition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+};
+
+window.addEventListener("keydown", playSound);
+const keys = document.querySelectorAll(".key");
+keys.forEach(keys => keys.addEventListener("transitionend", removeTrainsition));
