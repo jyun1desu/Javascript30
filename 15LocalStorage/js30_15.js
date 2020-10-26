2487647b68a7e8d5f55964c789835be19e5f59e3
jyun1 <jyuny12345@gmail.com>
2020-10-26 15:03:18 +0800

    clear all / check all / uncheck all function add

--- 15LocalStorage/js30_15.js
+++ 15LocalStorage/js30_15.js
@@ -1,6 +1,7 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
+const settings = document.querySelector('.settings');
 
 
 function addItem(e) {
@@ -27,18 +28,38 @@ function populateList(plates = [], platesList) {
             `
         }
     ).join("")
-
 }
 
 function toggleDone(e) {
     const element = e.target
     if (!element.matches('input')) return; //skip unless it's an input
-    const index = element.dataset.index; 
+    const index = element.dataset.index;
     items[index].done = !items[index].done;
     localStorage.setItem('items', JSON.stringify(items));
-    populateList(items, itemsList);
+}
+
+function setting(e) {
+    const checkboxes = itemsList.querySelectorAll('input[type="checkbox"]')
+    switch (e.target.name) {
+        case ("clear_all"):
+            localStorage.removeItem('items');
+            items.splice(0, items.length);
+            populateList(items, itemsList);
+            break;
+        case ("check_all"):
+            items.forEach(item => item.done = true);
+            checkboxes.forEach(checkbox => checkbox.checked = true);
+            localStorage.setItem('items', JSON.stringify(items));
+            break;
+        case ("uncheck_all"):
+            items.forEach(item => item.done = false);
+            checkboxes.forEach(checkbox => checkbox.checked = false);
+            localStorage.setItem('items', JSON.stringify(items));
+            break;
+    }
 }
 
 populateList(items, itemsList);
 addItems.addEventListener("submit", addItem);
 itemsList.addEventListener("click", toggleDone);
+settings.addEventListener("click", setting)
